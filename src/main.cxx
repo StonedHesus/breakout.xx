6661979e696f7e77c25ff7ab2b8e72afaa38ed97
StonedHesus <andrei.ionescu@etu.u-paris.fr>
2024-11-14 12:49:16 +0100

    Concluded the issue.

--- src/main.cxx
+++ src/main.cxx
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <cmath>
+#include <optional>
+
+
+class Paddle {
+	private:
+		double x;
+		double y;
+		double width{20.0};
+		double height{5.0};
+		std::optional<int> durability{std::nullopt};// By default, the standard paddle never breaks, but fancy ones will
+	public:
+		Paddle() noexcept = default;
+		Paddle(double x, double y) : x{x}, y{y} {}
+		~Paddle() = default;
+
+	friend std::ostream& operator<<(std::ostream& os, const Paddle& paddle) {
+		os << "Paddle[x=" << paddle.x << ", y=" << paddle.y
+			<< ", width=" << paddle.width << ", height=" << paddle.height;
+		if (paddle.durability) {
+			os << ", durability=" << *paddle.durability;
+		} else {
+			os << ", standard paddle (unbreakable)";
+		}
+		os << "]";
+		return os;
+	}
+};
 
 int main(void) {
-	std::cout << "Hello world!" << std::endl;
+	auto p = new Paddle(2.4, 3.7);
+	std::cout << *p;
+	delete p;
 	return EXIT_SUCCESS;
 }
